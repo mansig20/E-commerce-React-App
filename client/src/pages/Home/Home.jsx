ef992b0330399b18ff74adeb4c937b45c0b7ae83
mansig20 <mansigehlot.2021cs@technonjr.org>
2024-07-30 23:20:54 +0530

    Added Slider functionality & designed Featured Products with product card

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Slider from "../../components/Slider/Slider";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="home">
+      <Slider />
+      <FeaturedProducts type="featured" />
+      <FeaturedProducts type="trending" />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
